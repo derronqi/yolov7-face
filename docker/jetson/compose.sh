97750f1fe749adbbd3682ca57e2c52d0f0a09903
pdh <pdh930105@gmail.com>
2023-02-10 13:17:34 +0900

    modified Docker images

--- docker/jetson/compose.sh
+++ docker/jetson/compose.sh
@@ -8,11 +8,12 @@ printf "USERNAME=%s\n" $USERNAME
 xhost +
 xhost +local:docker
 echo $xhost
-docker-compose rm -f 
+docker rm -f ${CONTAINERNAME}
 #docker-compose up -d 
 #docker-compose up -d --force-recreate --no-deps --build --no-cache
 #docker-compose rm -f
-docker-compose up -d --build 
+docker-compose build #--no-cache
+docker-compose up -d
 
 docker exec ${CONTAINERNAME} echo "xhost's display : "
 docker exec ${CONTAINERNAME} echo $DISPLAY
