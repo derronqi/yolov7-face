70e723d9c28f384226400f80c74809d9dfa778ba
pdh <pdh930105@gmail.com>
2023-02-27 16:21:08 +0000

    test_cv2_show code update

---
+++ test_camera/test_cv2_imshow.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2
+
+# detect all connected webcams
+img = cv2.imread('./image.jpg')
+
+cv2.imshow('webcam', img)
+k = cv2.waitKey(1)
+if k == ord('q') or k == 27:
+    cv2.destroyAllWindows()
