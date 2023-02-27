5b30e6862dc3257c8d48722d17092f54eb70f432
pdh <pdh930105@gmail.com>
2023-02-27 16:22:27 +0000

    docker update

--- docker/gpu/compose.sh
+++ docker/gpu/compose.sh
@@ -9,7 +9,10 @@ xhost +
 xhost +local:docker
 echo $xhost
 #docker-compose up -d --force-recreate --no-deps --build
-docker-compose rm -f 
+docker rm -f ${CONTAINERNAME}
 docker-compose up -d --build 
-docker-compose exec echo $xhost
+docker exec ${CONTAINERNAME} echo "xhost's display : "
+docker exec ${CONTAINERNAME} echo $DISPLAY
+docker exec ${CONTAINERNAME} echo "visualize test"
+docker exec ${CONTAINERNAME} xclock
 docker exec -it ${CONTAINERNAME} /bin/bash
