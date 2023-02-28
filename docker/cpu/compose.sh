b79388024ddd089a23682df2d875a4a384af3615
pdh <pdh930105@dgist.ac.kr>
2023-02-28 10:43:05 +0900

    update docker error & cpu compose.sh and test cv2

--- docker/cpu/compose.sh
+++ docker/cpu/compose.sh
@@ -3,14 +3,14 @@ export GROUPID=$(id -g)
 export USERNAME=$(whoami)
 export CONTAINERNAME="yolo_2210_pdh"
 printf "USERID=%s\n" $USERID 
-printf "GROUPID=%s\n" $GROUPID
+printf "GROUPID=%s\n" $GROUPID 
 printf "USERNAME=%s\n" $USERNAME 
 xhost +
 xhost +local:docker
 echo $xhost
 #docker-compose up -d --force-recreate --no-deps --build
 docker rm -f ${CONTAINERNAME}
-docker-compose up -d --build
+docker-compose up -d --build  --force-recreate
 docker exec ${CONTAINERNAME} echo "xhost's display : "
 docker exec ${CONTAINERNAME} echo $DISPLAY
 docker exec ${CONTAINERNAME} echo "visualize test"
--- setting_test/test_cv2_imshow.py
+++ setting_test/test_cv2_imshow.py
@@ -5,6 +5,8 @@ import cv2
 img = cv2.imread('./image.jpg')
 
 cv2.imshow('webcam', img)
-k = cv2.waitKey(1)
-if k == ord('q') or k == 27:
-    cv2.destroyAllWindows()
+while True:
+    k = cv2.waitKey(1)
+    if k == ord('q') or k == 27:
+        cv2.destroyAllWindows()
+        break
