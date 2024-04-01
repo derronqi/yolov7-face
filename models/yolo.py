e71ca53d1faf8c8b9e6858749f94c94fab24cee4
X00_123 <quyang@qiyi.com>
2024-04-01 14:21:34 +0800

    fix bug for multilabel training

--- models/yolo.py
+++ models/yolo.py
@@ -272,8 +272,8 @@ class IKeypoint(nn.Module):
 
             bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
             x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
-            x_det = x[i][..., :6].clone().detach()
-            x_kpt = x[i][..., 6:].clone().detach()
+            x_det = x[i][..., :5+self.nc].clone().detach()
+            x_kpt = x[i][..., 5+self.nc:].clone().detach()
 
             if not self.training:  # inference
                 if self.grid[i].shape[2:4] != x[i].shape[2:4]:
@@ -295,12 +295,13 @@ class IKeypoint(nn.Module):
                         x_kpt[..., 2::3] = x_kpt[..., 2::3].sigmoid()
 
                     y = torch.cat((xy, wh, y[..., 4:], x_kpt), dim = -1)
-
+     
                 else:  # for YOLOv5 on AWS Inferentia https://github.com/ultralytics/yolov5/pull/2953
                     xy = (y[..., 0:2] * 2. - 0.5 + self.grid[i]) * self.stride[i]  # xy
                     wh = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
                     if self.nkpt != 0:
                         y[..., 6:] = (y[..., 6:] * 2. - 0.5 + self.grid[i].repeat((1,1,1,1,self.nkpt))) * self.stride[i]  # xy
+            
                     y = torch.cat((xy, wh, y[..., 4:]), -1)
 
                 z.append(y.view(bs, -1, self.no))
